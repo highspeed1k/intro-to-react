2ec1036c2135df59fc3af63d0d759eadaa47a145
Highspeed1k <spencerpaulbrown@gmail.com>
2018-08-20 16:39:57 -0500

    state-and-props-checkpoint

--- react-to-to/src/App.js
+++ react-to-to/src/App.js
@@ -13,7 +13,7 @@ class App extends Component{
      ]
    };
  }
-                                                                        
+
   render() {
     return (
       <div className="App">
@@ -28,4 +28,4 @@ class App extends Component{
   }
 }
 
-export default App;
+ export default App;
